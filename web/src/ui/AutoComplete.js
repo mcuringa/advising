25c5fae91755ef1db83d723b5c0b2688e6d21e82
mxc <matt@curinga.com>
2020-10-15 15:04:56 -0400

    working on autocomplete, not done

--- web/src/ui/AutoComplete.js
+++ web/src/ui/AutoComplete.js
@@ -1,6 +1,10 @@
 import React from "react";
 import _ from "lodash";
+import classNames from 'classnames/bind';
+import { createPopper } from '@popperjs/core';
 
+
+import "./AutoComplete.css";
 import {
   TextInput
 } from "./form-ui";
@@ -12,8 +16,17 @@ class AutoComplete extends React.Component {
   constructor(props) {
     super(props);
     this.popId  = _.uniqueId("drop_");
-    console.log("loaded autocomplete")
   }
+
+
+  componentDidMount() {
+    const popcorn = document.getElementById(this.popId);
+    const tip = popcorn.querySelector(".complete-options");
+    let popper = createPopper(popcorn, tip, {
+      placement: "bottom-start",
+    });
+  }
+
 
 
 
@@ -36,33 +49,32 @@ class AutoComplete extends React.Component {
       return f;
     }
 
+
+    const weightItems = (i)=>{
+
+    }
+
+    items = _.filter(items,i=>i.toLowerCase().includes(value.toLowerCase()));
+
+    let showOptions = items.length;
+
     const data = _.map(items, (i, key)=><Item key={key} val={i} onClick={makeChange(i)} />)
+    let css = classNames("list-group w-100 complete-options", {"d-none": !showOptions})
 
     return (
-      <div className="input-group">
-        <TextInput className="form-control-sm" id={id} value={value} onChange={onChange} {...props} />
-        <div className="input-group-append">
-          <div className="dropdown-menu" aria-labelledby={this.dropId}>
+      <div id={this.popId} className="input-group">
+        <TextInput className="form-control-sm rounded-0 rounded-top" id={id} value={value} onChange={onChange} {...props} />
+        <div className={css}>
             {data}
-          </div>
-          <button id={this.dropId}
-            type="button"
-            className="btn input-group-text btn-sm dropdown-toggle"
-            data-toggle="dropdown"
-            data-offset="-80%p"
-            aria-haspopup="true" aria-expanded="false" >
-              <span className="sr-only">Toggle Autocomplete</span>
-            </button>
         </div>
       </div>
     )
   }
 }
 
-
 function Item (props) {
   return (
-      <button className="btn btn-link dropdown-item"
+      <button className="list-group-item list-group-item-action p-0 m-0"
         type="button"
         onClick={props.onClick}>{props.val}</button>
   )
